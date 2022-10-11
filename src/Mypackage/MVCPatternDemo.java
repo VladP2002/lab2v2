0260cbb672f534a88bd2bab538c35ffda4ee68eb
Vlad <vlad.pasca17@gmail.com>
2022-10-11 13:38:08 +0300

    lab2 v2

--- src/Mypackage/MVCPatternDemo.java
+++ src/Mypackage/MVCPatternDemo.java
@@ -19,6 +19,27 @@ public class MVCPatternDemo {
         System.out.println("\nAfter updating, Car Details are as follows");
 
         controller.updateView();
+
+        System.out.println("\n");
+
+        Car car2 = new Car();
+        car2.setcarMark("Audi");
+        car2.setCarConstrYear(2015);
+        car2.setCarColour("Green");
+
+        Car car3 = new Car();
+        car3.setcarMark("Tesla");
+        car3.setCarConstrYear(2019);
+        car3.setCarColour("White");
+
+
+        Car[] cars = {model, car2, car3};
+        Car carFromArray = new Car();
+        carFromArray.setcarMark(cars[0].getcarMark());
+        carFromArray.setCarConstrYear(cars[1].getCarConstrYear());
+        carFromArray.setCarColour(cars[2].getCarColour());
+
+        view.printCarDetails(carFromArray.getcarMark(),carFromArray.getCarConstrYear(),carFromArray.getCarColour());
     }
 
     private static Car retriveCarFromDatabase(){
